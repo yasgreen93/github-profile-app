ee8b9b960427525af1c7ca07f4ddf266f9777b2d
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-04-21 13:24:01 +0100

    TDD displaying a list of users with dummy objects, tests passing

--- app/js/controllers/GithubProfileController.js
+++ app/js/controllers/GithubProfileController.js
@@ -2,5 +2,7 @@ angular
   .module('githubProfileApp')
   .controller('GithubProfileController', function() {
 
-    this.user = 'Kyle';
+    var self = this;
+
+    self.users = [{username: 'kyle', numOfRepos: 10, numOfFollowers: 20, avatar: 'kyle.jpg'}, {username: 'harsheet', numOfRepos: 18, numOfFollowers: 23, avatar: 'harsheet.jpg'}];
   });
--- test/e2e/githubProfileFeatures.js
+++ test/e2e/githubProfileFeatures.js
@@ -3,4 +3,11 @@ describe('Github Profile App', function() {
     browser.get('/');
     expect(browser.getTitle()).toEqual('Github Profile');
   });
+
+  it('displays a list of users', function() {
+    browser.get('/');
+    var users = $$("#users ul li");
+    expect(users.first().getText()).toEqual("Username: kyle, Repositories: 10, Followers: 20, Avatar: kyle.jpg");
+    expect(users.last().getText()).toEqual("Username: harsheet, Repositories: 18, Followers: 23, Avatar: harsheet.jpg");
+  });
 });
