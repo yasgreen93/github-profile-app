51ad4e4f28fff94f995e89753146dcba6387bf35
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-04-21 15:29:57 +0100

    Injected factory into tests and controller

--- app/js/controllers/GithubProfileController.js
+++ app/js/controllers/GithubProfileController.js
@@ -1,9 +1,20 @@
 angular
   .module('githubProfileApp')
-  .controller('GithubProfileController', function() {
+  .controller('GithubProfileController', ['userFactory', function(userFactory) {
 
     var self = this;
+    var user1, user2;
 
-    self.users = [{ username: 'kyle', numOfRepos: 10, numOfFollowers: 20, avatar: 'kyle.jpg' },
-                  { username: 'harsheet', numOfRepos: 18, numOfFollowers: 23, avatar: 'harsheet.jpg' }];
-  });
+    self.users = [user1 = new userFactory(), user2 = new userFactory()];
+
+    user1.username = 'kyle';
+    user1.numOfRepos = 10;
+    user1.numOfFollowers = 20;
+    user1.avatar = 'kyle.jpg';
+
+    user2.username = 'harsheet';
+    user2.numOfRepos = 18;
+    user2.numOfFollowers = 23;
+    user2.avatar = 'harsheet.jpg';
+
+  }]);
--- test/unit/GithubProfileController.spec.js
+++ test/unit/GithubProfileController.spec.js
@@ -1,15 +1,27 @@
 describe('GithubProfileController', function() {
   beforeEach(module('githubProfileApp'));
 
-  var ctrl;
+  var ctrl, userFactory;
 
-  beforeEach(inject(function($controller) {
+  beforeEach(inject(function($controller, _userFactory_) {
     ctrl = $controller('GithubProfileController');
+    userFactory = _userFactory_;
   }));
 
   it('has two users', function() {
-    var user1 = {username: 'kyle', numOfRepos: 10, numOfFollowers: 20, avatar: 'kyle.jpg'};
-    var user2 = {username: 'harsheet', numOfRepos: 18, numOfFollowers: 23, avatar: 'harsheet.jpg'};
+    var user1 = new userFactory();
+    user1.username = 'kyle';
+    user1.numOfRepos = 10;
+    user1.numOfFollowers = 20;
+    user1.avatar = 'kyle.jpg';
+
+    var user2 = new userFactory();
+    user2.username = 'harsheet';
+    user2.numOfRepos = 18;
+    user2.numOfFollowers = 23;
+    user2.avatar = 'harsheet.jpg';
+    
     expect(ctrl.users).toEqual([user1, user2]);
   });
+
 });
